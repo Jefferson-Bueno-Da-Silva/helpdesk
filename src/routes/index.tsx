1e20f3b39e55fbbe69527b86ba6768a16f8fd10a
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2022-01-25 22:16:42 -0300

    feat: create auth use cases

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 import { Home } from '@screens/Home';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
-  return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
-  );
+    const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+    useEffect(() => {
+        const subscribe = auth().onAuthStateChanged(setUser);
+
+        return subscribe;
+    }, []);
+
+    return <NavigationContainer>{user ? <Home /> : <AuthRoutes />}</NavigationContainer>;
 }
